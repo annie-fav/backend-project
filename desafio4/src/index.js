263136ef4d306a72947ee7e6c186e6a7eb30b95b
annie <fav.hala@gmail.com>
2023-02-21 19:12:47 -0300

    Desafio5

--- desafio4/src/index.js
+++ desafio4/src/index.js
@@ -3,7 +3,13 @@ import routerProduct from './routes/products_routes.js'
 import routerCart from './routes/carts_routes.js'
 import { __dirname } from "./path.js"
 import multer from "multer"
+import { engine } from 'express-handlebars';
+import * as path from 'path'
+import { Server } from "socket.io";
+import routerSocket from "./routes/socket.routes.js";
+import { ProductManager } from "./controllers/ProductManager.js";
 
+const productManager = new ProductManager('src/models/products.json');
 
 const storage = multer.diskStorage({
     destination: (req,file,cb) => {
@@ -18,17 +24,45 @@ const upload = multer ({storage: storage})
 
 const app = express();
 const PORT = 8080;
+const server = app.listen(PORT, () => {
+    console.log(`Server on http://localhost:${PORT}`)
+})
+
+//ServerIO
+const io = new Server(server)
+
+io.on("connection", async (socket) => { //io.on es cuando se establece la conexion
+    console.log("Cliente conectado")
+
+    socket.on("addProduct", async info => {//Cuando recibo informacion de mi cliente
+        socket.emit("msgAddProduct", await productManager.addProduct(info, ["/Users/annie/Desktop/Project Backend/desafio4/src/public/Img/Dress shoes man.webp"]))
+        socket.emit("getProducts", await productManager.getProducts())
+    })
+    
+
+    socket.on("deleteProduct", async id => {
+        socket.emit("msgDeleteProduct", await productManager.deleteProduct(parseInt(id)))
+        socket.emit("getProducts", await productManager.getProducts())
+    })
+
+    socket.emit("getProducts", await productManager.getProducts());
+})
 
 //Middlewares
 app.use(express.json())
 app.use(express.urlencoded({extende: true}))
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.resolve(__dirname, './views')); //__dirname + './views'
 
 //Routes
-app.use('/static', express.static(__dirname + '/public'))
+app.use('/', express.static(__dirname + '/public'))
+app.use('/', routerSocket)
+app.use('/realtimeproducts', routerSocket)
 app.use('/api/products', routerProduct)
 app.use('/api/carts', routerCart)
-app.post('/upload', upload.single('product'), (req, res) => {
-    console.log(req.file)
-    res.send('Uploaded image')
-})
+// app.post('/upload', upload.single('product'), (req, res) => {
+//     console.log(req.file)
+//     res.send('Uploaded image')
+// })
 
